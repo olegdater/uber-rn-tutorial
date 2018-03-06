cbc4312b816d728d11074f634e5386600c351562
Oleg <olegwn@gmail.com>
2018-03-06 11:44:07 +0300

    Install React Native Maps

---
+++ ios/Pods/Target Support Files/Pods-UberExercise/Pods-UberExercise-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UberExercise : NSObject
+@end
+@implementation PodsDummy_Pods_UberExercise
+@end
--- ios/UberExercise/AppDelegate.m
+++ ios/UberExercise/AppDelegate.m
@@ -11,11 +11,13 @@
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+@import GoogleMaps; //add this line if you want to use Google Maps
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"AIzaSyB3EgV8iL4N4vN_vfO9pY_VqSbUXFdh9Q0"];
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
